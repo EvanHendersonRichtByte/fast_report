051f2ca39af56321a61a6d122d50b3e25cb85877
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-04-27 03:11:48 +0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,6 @@ module.exports = {
         icon: `src/images/tribrata.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: "gatsby-plugin-pdf",
-      options: {
-        allPages: true,
-        outputPath: "/public/exports",
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
