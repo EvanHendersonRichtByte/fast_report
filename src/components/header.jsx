2b6d08b1214ed496055bb379380d6278f9ee2e7a
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-04-27 14:28:53 +0700

    update input state

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,23 +2,27 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <ul>
-      <li>
-        <h1>
-          <Link>{siteTitle}</Link>
-        </h1>
-      </li>
-      <li>
-        <Link>Hasil</Link>
-      </li>
-      <li>
-        <Link>Settings</Link>
-      </li>
-    </ul>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const print = () => {
+    document.querySelector("header").classList.add("hidden")
+    window.print()
+    window.location.reload()
+  }
+  return (
+    <header>
+      <ul>
+        <li>
+          <h1>
+            <Link>{siteTitle}</Link>
+          </h1>
+        </li>
+        <li>
+          <button onClick={print}>Cetak</button>
+        </li>
+      </ul>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,45 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
+import { useState } from "react"
+import { useEffect } from "react"
 const IndexPage = () => {
+  const [state, setState] = useState({
+    header: {
+      head1: "KEPOLISIAN NEGARA REPUBLIK INDONESIA",
+      head2: "DAERAH KEPULAUAN JATIM",
+      head3: "RESOR LINGGA",
+      head4: "Jalan Batu Kacang 01 Dabo Singkep",
+      title1: "LAPORAN HASIL KEGIATAN",
+      title2:
+        "SIMPATIK GO TO SCHOOL PERSONIL SPKT POLRES LINGGA DALAM RANGKA MENINGKATKAN MINAT BACA ANAK DAN MENCIPTAKAN POLISI SAHABAT ANAK",
+      date1: "DABO SINGKEP",
+      date2: "25 FEBRUARI 2020",
+    },
+  })
+
+  const handleChange = e => {
+    setState(state => ({
+      ...state.header,
+      header: { [e.target.name]: e.target.value },
+    }))
+  }
+
+  useEffect(() => {
+    // Dealing with Textarea Height
+    function calcHeight(value) {
+      let numberOfLineBreaks = (value.match(/\n/g) || []).length
+      // min-height + lines x line-height + padding + border
+      let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2
+      return newHeight
+    }
+
+    let textarea = document.querySelector("textarea")
+    textarea.addEventListener("keyup", () => {
+      textarea.style.height = calcHeight(textarea.value) + "px"
+    })
+  }, [state])
+
   return (
     <Layout>
       <Seo title="Home" />
@@ -10,32 +48,77 @@ const IndexPage = () => {
         <div className="index__left">
           <div className="index__left--cover">
             <div className="header">
-              <p>KEPOLISIAN NEGARA REPUBLIK INDONESIA</p>
-              <p>DAERAH KEPULAUAN RIAU</p>
-              <p>RESOR LINGGA</p>
-              <p>Jalan Batu Kacang 01 Dabo Singkep</p>
+              <input
+                type="text"
+                name="head1"
+                value={state.header.head1}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="head2"
+                value={state.header.head2}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="head3"
+                value={state.header.head3}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="head4"
+                value={state.header.head4}
+                onChange={handleChange}
+              />
             </div>
             <div className="logo">
-              <StaticImage src="../images/cover.png" alt="Tribrata" />
+              <StaticImage
+                src="../images/cover.png"
+                alt="Tribrata"
+                height={"100%"}
+              />
             </div>
             <div className="description">
               <p>
-                LAPORAN HASIL KEGIATAN SIMPATIK GO TO SCHOOL PERSONIL SPKT
-                POLRES LINGGA DALAM RANGKA MENINGKATKAN MINAT BACA ANAK DAN
-                MENCIPTAKAN POLISI SAHABAT ANAK
+                <textarea
+                  cols={50}
+                  name="title1"
+                  value={state.header.title1}
+                  onChange={handleChange}
+                />
+                <textarea
+                  cols={50}
+                  name="title2"
+                  value={state.header.title2}
+                  onChange={handleChange}
+                />
               </p>
             </div>
             <div className="decoration">
               <StaticImage src="../images/decoration.png" alt="Tribrata" />
             </div>
             <div className="date">
-              <h3 className="location">DABO SINGKEP</h3>,
-              <h3 className="time">25 FEBRUARI 2020</h3>
+              <input
+                className="location"
+                type="text"
+                name="date1"
+                value={state.header.date1}
+                onChange={handleChange}
+              />
+              ,
+              <input
+                className="time"
+                type="text"
+                name="date2"
+                value={state.header.date2}
+                onChange={handleChange}
+              />
             </div>
           </div>
         </div>
-        <div className="index__right">
-        </div>
+        {/* <div className="index__right"></div> */}
       </section>
     </Layout>
   )
